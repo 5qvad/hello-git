615d6c23c7a1e32aff8688a43b4179d8fa68df02
Alexandr Goncharov <bitproton@gmail.com>
2022-07-23 22:58:00 +0300

    add Main & readme

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String [] args){
+        int [] sales = {6, 4, 2, 2, 7, 4, 5};
+        SalesManager salesman = new SalesManager(sales);
+        System.out.println("Максимальные продажи:  " + salesman.max());
+    }
+}
